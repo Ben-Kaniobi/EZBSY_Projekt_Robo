69c01739dc9a97ca188623fa2f15a019b9a38208
Simon Plattner <plats1@bfh.ch>
2014-04-26 13:10:28 +0200

    Ready for first test

--- src/application/robot_left_task.c
+++ src/application/robot_left_task.c
@@ -53,11 +53,12 @@ uint8_t payloadLeft[ ][ 8 ] =
 {
 	{ 0x02, 0x80, 0x5B, 0xC9, 0xD0, 0xAA, 0x00, 0x00 },	/* Waiting position */
 	{ 0x02, 0x80, 0xA0, 0xD5, 0x70, 0x87, 0x00, 0x00 },	/* PrepareToTakeEcts */
-	{ 0x02, 0x80, 0x91, 0xD5, 0x70, 0xAA, 0x00, 0x00 },	/* LiftEctsUp */
-	{ 0x02, 0xCD, 0x91, 0xC3, 0x85, 0xAA, 0x00, 0x00 },	/* RotateToUnloadTray */
+//	{ 0x02, 0x80, 0x91, 0xD5, 0x70, 0xAA, 0x00, 0x00 },	/* LiftEctsUp */
+	{ 0x02, 0x80, 0x86, 0xDC, 0x85, 0xAA, 0x00, 0x00 },	/* LiftEctsUp */
+	{ 0x02, 0xCD, 0x86, 0xDC, 0x85, 0xAA, 0x00, 0x00 },	/* RotateToUnloadTray */
 	{ 0x02, 0xCD, 0x9F, 0xC3, 0x85, 0xAA, 0x00, 0x00 },	/* PrepareToUnloadEcts */
-	{ 0x02, 0xCD, 0x9F, 0xC3, 0x85, 0x87, 0x00, 0x00 },	/* UnloadEctsToTray */
-	{ 0x02, 0xCD, 0x91, 0xC3, 0x85, 0xAA, 0x00, 0x00 },	/* RemoveArmFromUnloadTray */
+	{ 0x02, 0xCD, 0x9C, 0xBB, 0x85, 0x87, 0x00, 0x00 },	/* UnloadEctsToTray */
+	{ 0x02, 0xCD, 0x9C, 0xBB, 0x85, 0x87, 0x00, 0x00 },	/* RemoveArmFromUnloadTray */
 	{ 0x02, 0x80, 0x9C, 0xBB, 0x85, 0x87, 0x00, 0x00 }	/* RotateToConveyor */
 };
 
@@ -102,20 +103,23 @@ void vRobotLeftTask( void *pvParameters )
 	/* Wait 2000ms before starting to work */
     vTaskDelay( 2000 / portTICK_RATE_MS );
 
+    /* Give unloading tray semaphore free */
+    xSemaphoreGive( xSemaphoreEctsTray );
+
 	/* Prepare robot to take up an ECTS */
 	createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ PrepareToTakeEcts ] );
-    vTaskDelay( 1000 / portTICK_RATE_MS );
+    vTaskDelay( 1500 / portTICK_RATE_MS );
 
     for( ;; )
     {
 
     	/* Prepare robot to take up an ECTS */
     	createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ PrepareToTakeEcts ] );
-        vTaskDelay( 1000 / portTICK_RATE_MS );
+        vTaskDelay( 500 / portTICK_RATE_MS );
 
 
     	/* Wait for an ECTS that can be taken */
-        xSemaphoreTake( xSemaphoreLeftECTS, portMAX_DELAY );
+//        xSemaphoreTake( xSemaphoreLeftECTS, portMAX_DELAY );
 
 
         /* Check which ECTS to handle */
@@ -123,57 +127,58 @@ void vRobotLeftTask( void *pvParameters )
         find_ECTS(ECTS, conveyor_L);
 
         /* Initialize standard grab gesture */
-        uint8_t xEctsGrabPosition[ 8 ] = { 0x02, 0x80, 0xA0, 0xD5, 0x70, 0xAA, 0x00, 0x00 };
+        uint8_t xEctsGrabPosition[ 8 ] = { 0x02, 0x80, 0xA0, 0xD5, 0x70, 0x87, 0x00, 0x00 };
 
         /* Choose which position the ECTS actually has and adjust grab gesture accordingly */
-        switch( ECTS->y )
+//        switch( ECTS->y )
+        switch( 0 )
         {
         	case 0:
-				xEctsGrabPosition[2] = 0xA0;
-				xEctsGrabPosition[3] = 0xD5;
-				xEctsGrabPosition[4] = 0x70;
+        		xEctsGrabPosition[2] = 0x9A;
+        		xEctsGrabPosition[3] = 0xDA;
+        		xEctsGrabPosition[4] = 0x73;
 				break;
 
         	case 1:
-				xEctsGrabPosition[2] = 0xA0;
-				xEctsGrabPosition[3] = 0xD5;
-				xEctsGrabPosition[4] = 0x70;
+				xEctsGrabPosition[2] = 0x9A;
+				xEctsGrabPosition[3] = 0xD4;
+				xEctsGrabPosition[4] = 0x78;
 				break;
 
         	case 2:
-				xEctsGrabPosition[2] = 0xA0;
-				xEctsGrabPosition[3] = 0xD5;
-				xEctsGrabPosition[4] = 0x70;
+				xEctsGrabPosition[2] = 0x9D;
+				xEctsGrabPosition[3] = 0xD0;
+				xEctsGrabPosition[4] = 0x7A;
 				break;
 
         	case 3:
-				xEctsGrabPosition[2] = 0xA0;
-				xEctsGrabPosition[3] = 0xD5;
-				xEctsGrabPosition[4] = 0x70;
+				xEctsGrabPosition[2] = 0x9F;
+				xEctsGrabPosition[3] = 0xCB;
+				xEctsGrabPosition[4] = 0x7C;
 				break;
 
         	case 4:
-				xEctsGrabPosition[2] = 0xA0;
-				xEctsGrabPosition[3] = 0xD5;
-				xEctsGrabPosition[4] = 0x70;
+				xEctsGrabPosition[2] = 0xA1;
+				xEctsGrabPosition[3] = 0xC6;
+				xEctsGrabPosition[4] = 0x7F;
 				break;
 
         	case 5:
-				xEctsGrabPosition[2] = 0xA0;
-				xEctsGrabPosition[3] = 0xD5;
-				xEctsGrabPosition[4] = 0x70;
+				xEctsGrabPosition[2] = 0xA4;
+				xEctsGrabPosition[3] = 0xC1;
+				xEctsGrabPosition[4] = 0x81;
 				break;
 
         	case 6:
-				xEctsGrabPosition[2] = 0xA0;
-				xEctsGrabPosition[3] = 0xD5;
-				xEctsGrabPosition[4] = 0x70;
+				xEctsGrabPosition[2] = 0xA7;
+				xEctsGrabPosition[3] = 0xBC;
+				xEctsGrabPosition[4] = 0x84;
 				break;
 
         	case 7:
-				xEctsGrabPosition[2] = 0xA0;
-				xEctsGrabPosition[3] = 0xD5;
-				xEctsGrabPosition[4] = 0x70;
+				xEctsGrabPosition[2] = 0xAC;
+				xEctsGrabPosition[3] = 0xAF;
+				xEctsGrabPosition[4] = 0x8B;
 				break;
 
 			default:	/* If the position is not valid */
@@ -183,20 +188,25 @@ void vRobotLeftTask( void *pvParameters )
 				continue;	/* for */
         }
 
-    	/* Grab the ECTS */
+    	/* Prepare to grab the ECTS */
+    	createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, xEctsGrabPosition );
+        vTaskDelay( 600 / portTICK_RATE_MS );
+
+        /* Grab the ECTS */
+        xEctsGrabPosition[5] = 0xAA;
     	createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, xEctsGrabPosition );
 
     	/* Update ECTS position */
     	update_ECTS_z( robo_L );
 
-        vTaskDelay( 1000 / portTICK_RATE_MS );
+        vTaskDelay( 800 / portTICK_RATE_MS );
 
 
 
 
     	/* Lift the ECTS off the conveyor */
     	createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ LiftEctsUp ] );
-        vTaskDelay( 1000 / portTICK_RATE_MS );
+        vTaskDelay( 400 / portTICK_RATE_MS );
 
 
     	/* Release semaphore that locks the conveyor from moving */
@@ -205,7 +215,7 @@ void vRobotLeftTask( void *pvParameters )
 
     	/* Rotate to the unload tray */
     	createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ RotateToUnloadTray ] );
-        vTaskDelay( 2000 / portTICK_RATE_MS );
+        vTaskDelay( 1500 / portTICK_RATE_MS );
 
 
     	/* Claim the drop off tray */
@@ -214,27 +224,22 @@ void vRobotLeftTask( void *pvParameters )
 
     		/* Prepare to unload ECTS */
 			createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ PrepareToUnloadEcts ] );
-		    vTaskDelay( 1000 / portTICK_RATE_MS );
+		    vTaskDelay( 1200 / portTICK_RATE_MS );
 
 
 			/* Unload ECTS */
 			createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ UnloadEctsToTray ] );
-		    vTaskDelay( 1000 / portTICK_RATE_MS );
+		    vTaskDelay( 600 / portTICK_RATE_MS );
 
 		    /* Update ECTS location */
 		    update_ECTS_z( conveyor_C );
 
-			/* Remove robot arm from the drop off tray */
-			createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ RemoveArmFromUnloadTray ] );
-		    vTaskDelay( 1000 / portTICK_RATE_MS );
+			/* Remove robot arm from the drop off tray by rotating back to the conveyor */
+			createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ RotateToConveyor ] );
+		    vTaskDelay( 1200 / portTICK_RATE_MS );
 
     	}
     	xSemaphoreGive( xSemaphoreEctsTray );
-
-
-    	/* Rotate back to the conveyor */
-    	createCANMessage( CAN_ID_LEFT_ROBOT, CAN_INSTRUCTION_DLC, payloadLeft[ RotateToConveyor ] );
-        vTaskDelay( 2000 / portTICK_RATE_MS );
     }
 }
 
